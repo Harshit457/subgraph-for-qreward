0a404f1878ef66804958e87e6faf8cb924c6bfc7
Harshit Saini <saini457harshit@gmail.com>
2025-07-20 11:43:52 +0530

    Prize claimed added

--- src/contractgraph.ts
+++ src/contractgraph.ts
@@ -1,9 +1,17 @@
 import { CampaignCreated  }  from "../generated/contractgraph/contractgraph";
+import { PrizeClaimed } from "../generated/contractgraph/contractgraph";
 import { CampaignCreated as CampaignCreatedEntity } from "../generated/schema";
+import {PrizeClaimed as PrizeClaimedEntity} from "../generated/schema"
 export function handleCampaignCreated(event: CampaignCreated): void{
   const entity = new CampaignCreatedEntity(event.transaction.hash);
   entity.campaignId = event.params.campaignId;
   entity.company = event.params.company;
   entity.totalFunds = event.params.totalFunds;
+  entity.save();
+}
+export function handlePrizeClaimed(event: PrizeClaimed): void {
+  const entity = new PrizeClaimedEntity(event.transaction.hash);
+  entity.campaignId = event.params.campaignId;
+  entity.winner = event.params.winner;
   entity.save();
 }
